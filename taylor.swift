64bbc71b7cad559c2e3ee4ff0c6e72c090092878
TechClub <TechClub@NDVYD-iMac.local>
2018-03-01 19:12:32 -0800

    firstfolder

---
+++ taylor.swift
@@ -0,0 +1,13 @@
+print("Hello world")
+print(2+4)
+class StrangeAdding {
+   var mult1:Int = 3
+   var mult2:Int = 45
+
+    func complete () {
+        print(mult1*mult2)
+    }
+
+}
+var obj = StrangeAdding()
+obj.complete ()
